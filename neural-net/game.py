2ceceea07cf788c67f38230429a5aa3b3dce0b6b
Iridum-png <baker-e16@boswells-school.com>
2022-06-21 09:51:27 +0100

    Pieces can now move anywhere they like

--- neural-net/game.py
+++ neural-net/game.py
@@ -1,3 +1,5 @@
+import os
+
 class Board:
     def __init__(self):
         # Create board with following structure, with each piece being an instance of the Piece class
@@ -23,6 +25,8 @@ class Board:
         self.board.append([Piece('B', 7, i, order[i]) for i in range(8)])
 
     def output(self):
+        print("\n")
+        os.system('cls' if os.name == 'nt' else 'clear')
         # Printing the board using a reverse order so that the white pieces are on the bottom
         for i, row in enumerate(self.board[::-1]):
             print(" +----+----+----+----+----+----+----+----+")
@@ -35,8 +39,27 @@ class Board:
                     print("    |", end='')
             print("\n", end='')
         print(" +----+----+----+----+----+----+----+----+")
-        print("    a    b    c    d    e    f    g    h")
+        print("    a    b    c    d    e    f    g    h", end='')
 
+    def move(self):
+        chars = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        current = ['', '']
+        target = ['', '']
+        start = list(input("Enter the starting position (eg a1): ").strip())
+        end = list(input("Enter the ending position (eg h8): ").strip())
+        # Format the input to be in the correct format for list references
+        current[1], target[1] = chars[start[0]], chars[end[0]]
+        current[0], target[0] = int(start[1])-1, int(end[1])-1
+
+        if self.check():
+            self.board[target[0]][target[1]] = self.board[current[0]][current[1]]
+            self.board[current[0]][current[1]] = '  '
+        else:
+            print("Invalid move")
+            self.move()
+
+    def check(self) -> bool:
+        return True
 
 class Piece:
     def __init__(self, colour, x, y, type):
@@ -45,9 +68,29 @@ class Piece:
         self.y = y
         self.type = type
 
-    def move(self, x, y):
+    def make_move(self, x, y):
         pass
 
 
-game = Board()
-game.output()
+def check_win(board) -> bool:
+    from random import choice
+    return choice([True, False])
+
+
+def main():
+    board = Board()
+    won = False
+    move_num = 0
+    while not won:
+        move_num += 1
+        board.output()
+        print(f"\tMove {move_num} | White")
+        board.move()
+        board.output()
+        print(f"\tMove {move_num} | Black")
+        board.move()
+        check_win(board)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+if __name__ == "__main__":
+    main()
