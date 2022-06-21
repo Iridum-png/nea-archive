22db2e33497352cba35109ca5b1c184d995f1974
Iridum-png <baker-e16@boswells-school.com>
2022-06-21 22:27:11 +0100

    misc

---
+++ chess-rust/target/debug/deps/chess_rust-6d86e8696a03fbf6.d
@@ -0,0 +1,5 @@
+/Users/edwardbaker/Documents/nea/chess-rust/target/debug/deps/chess_rust-6d86e8696a03fbf6.rmeta: src/main.rs
+
+/Users/edwardbaker/Documents/nea/chess-rust/target/debug/deps/chess_rust-6d86e8696a03fbf6.d: src/main.rs
+
+src/main.rs:
