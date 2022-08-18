38957bd343858976c4b3fdfee0b55ec37b864cd5
Iridum-png <baker-e16@boswells-school.com>
2022-08-18 19:59:36 +0100

    Added ability to decode FEN strings into position

--- chess-react/src/App.tsx
+++ chess-react/src/App.tsx
@@ -2,7 +2,8 @@ import './App.css';
 import Chessboard from './components/chessboard/chessboard';
 
 function App() {
-  return (
+    // const defaultFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+    return (
     <div id="app">
       <Chessboard fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'/>
     </div>
--- chess-react/src/components/chessboard/chessboard.tsx
+++ chess-react/src/components/chessboard/chessboard.tsx
@@ -2,30 +2,86 @@ import './chessboard.css';
 import Tile from '../tile/tile';
 
 interface Props {
-    fen: string;
+    fen: string
 }
 
 interface Piece {
-    image: string;
-    x: number;
-    y: number;
+    image?: string
+    x: number
+    y: number
 }
+
+const pieces: Piece[] = [];
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]; // Array isn't needed here but is used for the sake of consistency and potential for modularity
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]; 
 
-function FenToPos({fen}: Props) {
-    // Convert fen string to board position
-    console.log(fen);
+function FenToPos(fen: string) {
+    const fenArr = fen.split('');
+    const fenReference: {[key: string]: string} = {r: 'rook_b', R: 'rook_w', n: 'knight_b', N: 'knight_w', b: 'bishop_b', B: 'bishop_w', q: 'queen_b', Q: 'queen_w', k: 'king_b', K: 'king_w', p: 'pawn_b', P: 'pawn_w' };
+    let temp1: string[] = [];
+    let temp2: string[][] = [];
+    let boardArray: string[] = [];
+    fenArr.forEach(f => {
+        if (isNumeric(f)) {
+            for (let i = 0; i < parseInt(f); i++) {
+                temp1.push(' ');
+            }
+        } else {
+            temp1.push(`assets/images/${fenReference[f]}.png`);
+        }
+    })
+    // very ugly code, but it works
+    // reverses array in chunks of 8 in order to render it properly
+    // TIDY UP NEEDED
+    for (let i = 0; i < 8; i++) {
+        temp2.push(temp1.slice(64 - (i + 1) * 8, 64 - i * 8));
+    }
+    temp2.forEach(t => {
+        t.forEach(e => {
+            boardArray.push(e);
+        })
+    })
+    return boardArray;
 }
 
-export default function Chessboard() {
+function isNumeric(str: any) {
+    return !isNaN(str);
+}   
+
+export default function Chessboard({ fen }: Props) {
     let board = [];
     
+    fen = '8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b';
+    const boardArray = FenToPos(fen.substring(0, fen.indexOf(' ')).replaceAll('/', '')); // Takes only the needed part to calculate the position of the board
+    
+    for (let i = 0; i < 64; i++) {
+        if (boardArray[i] === ' ') {
+            pieces.push({
+                image: undefined, 
+                x: i % 8,
+                y: Math.floor(i / 8)
+            });
+        } else {
+            pieces.push({
+            image:  boardArray[i], 
+            x: i % 8,
+            y: Math.floor(i / 8)
+        });
+        }
+    }
+
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
-        for (let i = 0; i < horizontalAxis.length; i++) { // Renders from top left to bottom right (a8 - h1)
+        for (let i = 0; i < horizontalAxis.length; i++) {
             const number = j + i + 2;
-            board.push(<Tile number={number}/>);
+            let image = undefined;
+
+            pieces.forEach(p => {
+                if (p.x === i && p.y === j) {
+                    image = p.image;
+                }
+            })
+            board.push(<Tile image={image} number={number}/>);
         }
     }
 
