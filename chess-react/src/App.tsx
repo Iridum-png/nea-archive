609a6c784645fa432bdd08cd6a07cbc8e9ace5b4
Iridum-png <baker-e16@boswells-school.com>
2022-08-18 17:00:27 +0100

    Turned each tile into its own component

--- chess-react/src/App.tsx
+++ chess-react/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Chessboard from './components/chessboard';
+import Chessboard from './components/chessboard/chessboard';
 
 function App() {
   return (
     <div id="app">
-      <Chessboard/>
+      <Chessboard fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'/>
     </div>
   );
 }
--- chess-react/src/components/chessboard/chessboard.tsx
+++ chess-react/src/components/chessboard/chessboard.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
 import './chessboard.css';
+import Tile from '../tile/tile';
+
+interface Props {
+    fen: string;
+}
+
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]; // Array isn't needed here but is used for the sake of consistency and potential for modularity
+
+function FenToPos({fen}: Props) {
+    // Convert fen string to board position
+    console.log(fen);
+}
 
 export default function Chessboard() {
     let board = [];
     
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
-        for (let i = 0; i < horizontalAxis.length; i++) { // Renders from top left to bottom right
+        for (let i = 0; i < horizontalAxis.length; i++) { // Renders from top left to bottom right (a8 - h1)
             const number = j + i + 2;
-
-            if (number % 2 == 0) {
-                board.push(<div className="tile black-tile"></div>);
-            } else {
-                board.push(<div className="tile white-tile"></div>);
-            }
+            board.push(<Tile number={number}/>);
         }
     }
 
