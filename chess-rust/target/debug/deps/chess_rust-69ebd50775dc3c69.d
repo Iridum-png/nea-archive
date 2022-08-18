a86137c00cd4213149b63c51ff8f357647dbb725
Iridum-png <baker-e16@boswells-school.com>
2022-08-18 12:35:47 +0100

    .

---
+++ chess-rust/target/debug/deps/chess_rust-69ebd50775dc3c69.d
@@ -0,0 +1,5 @@
+c:\Users\ed9ba\Documents\Coding\NEA\nea\chess-rust\target\debug\deps\chess_rust-69ebd50775dc3c69.rmeta: src\main.rs
+
+c:\Users\ed9ba\Documents\Coding\NEA\nea\chess-rust\target\debug\deps\chess_rust-69ebd50775dc3c69.d: src\main.rs
+
+src\main.rs:
---
+++ chess-rust/target/debug/deps/chess_rust-cb49b68c74b67ac6.d
@@ -0,0 +1,5 @@
+c:\Users\ed9ba\Documents\Coding\NEA\nea\chess-rust\target\debug\deps\chess_rust-cb49b68c74b67ac6.rmeta: src\main.rs
+
+c:\Users\ed9ba\Documents\Coding\NEA\nea\chess-rust\target\debug\deps\chess_rust-cb49b68c74b67ac6.d: src\main.rs
+
+src\main.rs:
