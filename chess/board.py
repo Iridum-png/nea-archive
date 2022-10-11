03d30cc24e26b00683065f41c9156d3ec210ec8e
Iridum-png <baker-e16@boswells-school.com>
2022-10-11 11:43:54 +0100

    Slight restructure based on changing piece classes

--- chess/board.py
+++ chess/board.py
@@ -1,5 +1,4 @@
-from piece import Piece
-from game import Game
+from piece import Piece, Pawn, Rook, Knight, Bishop, Queen, King, Empty
 
 class Board:
     def __init__(self):
@@ -7,35 +6,29 @@ class Board:
         self.turn = 'w'
 
     def loadFromFen(self, fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'):
-        piece_type_from_symbol = {'k': 'king', 'q': 'queen', 'r': 'rook', 'b': 'bishop', 'n': 'knight', 'p': 'pawn'}
+        piece_type_from_symbol = {'k': King, 'q': Queen, 'r': Rook, 'b': Bishop, 'n': Knight, 'p': Pawn}
         
-        fen_board = fen.split(' ')[0][::-1]
-        
-        column = 0
-        row = 7
+        fen_board = fen.split(' ')[0][::-1].split('/')
 
-        for symbol in fen_board:
-            if symbol == '/':
-                column = 0
-                row -= 1
-            elif symbol.isdigit():
-                column += int(symbol)
-                for _ in range(int(symbol)):
-                    self.board.append(Piece('none', 'none'))
-            else:
-                piece_colour = 'w' if symbol.isupper() else 'b'
-                piece_type = piece_type_from_symbol[symbol.lower()]
-                self.board.append(Piece(piece_colour, piece_type))
-                column += 1
+        for rows in fen_board:
+            for symbol in rows[::-1]:
+                if symbol.isdigit():
+                    for _ in range(int(symbol)):
+                        self.board.append(Empty())
+                else:
+                    piece_colour = 'w' if symbol.isupper() else 'b'
+                    piece_type = piece_type_from_symbol[symbol.lower()]
+                    self.board.append(piece_type(piece_colour))  
+        self.turn = fen.split(' ')[1]
 
     def printBoard(self):
         from os import system, name
-        # system('cls' if name == 'nt' else 'clear')
+        system('cls' if name == 'nt' else 'clear')
         print(" +--+--+--+--+--+--+--+--+")
         row_num = 8
         for i in range(7, -1, -1):
             print(str(row_num)+"|", end="")
-            for j in range(7, -1, -1):
+            for j in range(8):
                 current = self.board[i*8 + j]
                 try:
                     print(f"{current.getColour()}{current.getType()}|", end="")
@@ -43,18 +36,24 @@ class Board:
                     print("  |", end="")
             print("\n +--+--+--+--+--+--+--+--+")
             row_num -= 1
-        print(f"  a  b  c  d  e  f  g  h\t{self.turn}")
+        print(f"  a  b  c  d  e  f  g  h\t{self.turn.upper()}'s turn")
+
+    def debugPrint(self):
+        for i in range(8):
+            for j in range(8):
+                current = self.board[i*8 + j]
+                print(self.outputPiece(current), self.board.index(current), end=" ")
+        print()
 
     def move(self, start: tuple, end: tuple):
-        print(start, end)
-        working_index = start[0] + start[1]*8
+        working_index = start[0]*8 + start[1]
         working = self.board[working_index]
-        target_index = end[0] + end[1]*8
+        target_index = end[0]*8 + end[1]
         target = self.board[target_index]
 
-        if working.isValid(working, target, self.turn):
+        if working.isValid(working, target, working_index, target_index, self.turn):
             self.board[target_index] = working
-            self.board[working_index] = '    '
+            self.board[working_index] = Empty()
             self.turn = 'b' if self.turn == 'w' else 'w'
         else:
             print("Invalid move")
--- chess/game.py
+++ chess/game.py
@@ -1,11 +1,26 @@
 def process(coordinate: str) -> tuple:
-     # Convert board coordinates into list indexes
-    print(int(coordinate[1]) - 1, ord(coordinate[0]) - 97)
+     # Convert board coordinates into array index
     return (int(coordinate[1]) - 1, ord(coordinate[0]) - 97)
 
+def resetTurn(board) -> None:
+    board.printBoard()
+    turn(board)
+
 def turn(board) -> bool:
-    start = process(input("Enter start position (eg a1): "))
-    end = process(input("Enter end position (eg h8): "))
+    valid_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    start = input("Enter start position (eg a1): ")
+    try:
+        if start[0] in valid_letters:
+            start = process(start)
+        else:
+            resetTurn(board)
+        end = input("Enter end position (eg h8): ")
+        if end[0] in valid_letters:
+            end = process(end)
+        else:
+            resetTurn(board)
+    except IndexError:
+        resetTurn(board)
 
     board.move(start, end)
     return board.isWon()
@@ -17,6 +32,7 @@ def main():
     won = False
 
     while not won:
+        # board.debugPrint()
         board.printBoard()
         won = turn(board)
 
