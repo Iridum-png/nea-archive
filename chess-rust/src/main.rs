b36a58169229b48944514842ab97f6db0ba79cb7
Iridum-png <baker-e16@boswells-school.com>
2022-10-18 22:59:48 +0100

    Main file theoretically complete

--- chess-rust/src/main.rs
+++ chess-rust/src/main.rs
@@ -2,8 +2,10 @@ use board::Board;
 use std::{fs::File, io};
 mod board;
 
-fn process<'a>(coordinate: (&'a str, &'a str)) -> (&'a [u8], &'a [u8]) {
-    return (coordinate.1.as_bytes(), coordinate.0.as_bytes());
+fn process<'a>(coordinate: (&'a str, &'a str)) -> (i32, i32) {
+    let mut coord1: i32 = coordinate.1.parse().unwrap();
+    let mut coord2: i32 = coordinate.0.parse().unwrap();
+    return (coord1 - 1, coord2 - 97);
 }
 
 fn reset_turn(board: &Board) {
@@ -13,10 +15,37 @@ fn reset_turn(board: &Board) {
 
 fn turn(board: &board::Board) -> bool {
     let valid_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    let mut start;
+
+    // Take the input for starting position
+    println!("Enter start position (e.g. a1): ");
+    let mut start = String::new();
     io::stdin()
         .read_line(&mut start)
-        .expect("error: unable to read user input");
+        .expect("Error: unable to read user input");
+    // Check if the first character of start is in valid_letters
+    if !valid_letters.contains(&start.chars().nth(0).unwrap()) {
+        println!("Error: invalid start position");
+        reset_turn(board);
+    }
+    // Convert the input into useable coordinates
+    let start_pos = process(start.split_at(0));
+
+    // Take the input for starting position
+    println!("Enter end position (e.g. h8): ");
+    let mut end = String::new();
+    io::stdin()
+        .read_line(&mut end)
+        .expect("Error: unable to read user input");
+    // Check if the first character of start is in valid_letters
+    if !valid_letters.contains(&end.chars().nth(0).unwrap()) {
+        println!("Error: invalid start position");
+        reset_turn(board);
+    }
+    // Convert the input into useable coordinates
+    let end_pos = process(end.split_at(0));
+
+    board::Board::r#move(&board, start_pos, end_pos);
+    return board::Board::is_won(&board);
 }
 
 fn main() {
